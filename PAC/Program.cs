74947642291cfbf28d3dc9af5795cd2b521d00d1
SpataroPF <spataro.pf@gmail.com>
2026-03-31 11:52:52 +0200

    Premier test avec spectre

--- PAC/Program.cs
+++ PAC/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Spectre.Console;
+
+AnsiConsole.MarkupLine("[blue]Bienvenue ![/]");
+
+var nom = AnsiConsole.Ask<string>("Quel est ton [green]nom[/] ?");
+
+var choix = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Que veux-tu faire ?")
+        .AddChoices("Jouer", "Quitter"));
+
+if (choix == "Jouer")
+{
+    AnsiConsole.MarkupLine($"[yellow]Bienvenue {nom} dans le jeu ![/]");
+}
+else
+{
+    AnsiConsole.MarkupLine("[red]Au revoir ![/]");
+}
