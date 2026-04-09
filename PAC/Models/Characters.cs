ea7bc731e40dfd0698cfe0928d115f36f9d65d23
SpataroPF <spataro.pf@gmail.com>
2026-04-09 14:55:20 +0200

    folder hierarchy + new characters and player class

---
+++ PAC/Models/Characters.cs
@@ -0,0 +1,41 @@
+﻿namespace PAC.Models;
+
+public abstract class Character
+{
+    public string Name { get; private set; }
+    public int Health { get; private set; }
+    public int MaxHealth { get; private set; }
+    public int Attack { get; private set; }
+
+    public bool IsAlive => Health > 0;
+
+    public Character(string name, int health, int attack)
+    {
+        Name = name;
+        MaxHealth = health;
+        Health = health;
+        Attack = attack;
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        Health -= damage;
+
+        if (Health < 0)
+            Health = 0;
+    }
+
+    public void Heal(int amount)
+    {
+        Health += amount;
+
+        if (Health > MaxHealth)
+            Health = MaxHealth;
+    }
+
+    public void IncreaseMaxHealth(int amount)
+    {
+        MaxHealth += amount;
+        Health = MaxHealth;
+    }
+}
---
+++ PAC/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace PAC.Models;
+
+public class Player : Character
+{
+    public int Level { get; private set; }
+    public int Experience { get; private set; }
+
+    public Player(string name) : base(name, 100, 10)
+    {
+        Level = 1;
+        Experience = 0;
+    }
+
+    public void GainExperience(int amount)
+    {
+        Experience += amount;
+
+        if (Experience >= 100)
+        {
+            LevelUp();
+            Experience = 0;
+        }
+    }
+
+    private void LevelUp()
+    {
+        Level++;
+        IncreaseMaxHealth(10);
+    }
+}
